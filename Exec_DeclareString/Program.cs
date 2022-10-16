420e9775e22741efa791340be73aca72e51fa513
Yang <Yang@YANG-PC>
2022-10-16 19:12:05 +0800

    加入專案檔案。

---
+++ Exec_DeclareString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c:\\temp\\a.jpg");
+            Console.WriteLine("我是一個\"好學生\"");
+            Console.WriteLine("我是第一行\r\n我是第二行");
+        }
+    }
+}
